483ec00bbb9e054a912b558c778204a19c7c5831
zengxiangji <1245768745@qq.com>
2019-01-01 10:24:53 +0800

    circle Bean resolve

--- src/test/java/BeanFactoryTest.java
+++ src/test/java/BeanFactoryTest.java
@@ -1,9 +1,11 @@
 import org.junit.Test;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.util.Assert;
 import sourceStudy.springSourceStudy.com.model.MyTestBean;
+
 @SuppressWarnings("deprecation")
 public class BeanFactoryTest {
     @Test
@@ -11,5 +13,14 @@ public class BeanFactoryTest {
         BeanFactory bf = new XmlBeanFactory(new ClassPathResource("beanFactoryTest.xml"));
         MyTestBean myTestBean = (MyTestBean) bf.getBean("myTestBean");
         Assert.hasText("testStr",myTestBean.getTestStr());
+    }
+
+    @Test
+    public void testCircleByConstructor() throws Throwable{
+        try {
+            new ClassPathXmlApplicationContext("beanFactoryTest.xml");
+        }catch (Exception e){
+            throw e.getCause().getCause().getCause();
+        }
     }
 }

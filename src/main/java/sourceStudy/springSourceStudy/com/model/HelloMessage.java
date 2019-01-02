a8593c9a2d7ea71b2cea943bfbf72fd1035a8d10
zengxiangji <1245768745@qq.com>
2019-01-02 09:09:41 +0800

    beanFactory后处理

---
+++ src/main/java/sourceStudy/springSourceStudy/com/model/HelloMessage.java
@@ -0,0 +1,13 @@
+package sourceStudy.springSourceStudy.com.model;
+
+public class HelloMessage {
+    private String mes;
+
+    public String getMes() {
+        return mes;
+    }
+
+    public void setMes(String mes) {
+        this.mes = mes;
+    }
+}
--- src/test/java/BeanFactoryTest.java
+++ src/test/java/BeanFactoryTest.java
@@ -1,9 +1,11 @@
 import org.junit.Test;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.util.Assert;
+import sourceStudy.springSourceStudy.com.model.HelloMessage;
 import sourceStudy.springSourceStudy.com.model.MyTestBean;
 
 @SuppressWarnings("deprecation")
@@ -22,5 +24,22 @@ public class BeanFactoryTest {
         }catch (Exception e){
             throw e.getCause().getCause().getCause();
         }
+    }
+
+    @Test
+    public void testBeanFactory(){
+        // type:1
+        // 解析bean并注册进beanFactory
+//        BeanFactory bf = new XmlBeanFactory(new ClassPathResource("beanFactoryTest.xml"));
+        //获取bean的实例
+//        HelloMessage message = (HelloMessage) bf.getBean("message");
+//        System.out.println(message.getMes());
+        // type:1 只是加loadbean 并获取，没有进行beanFactory后处理
+        // mesage.getMes()的结果为变量${bean.message}，没有进行转换
+
+        //type:2
+        ApplicationContext context =  new ClassPathXmlApplicationContext("beanFactoryTest.xml");
+        HelloMessage message = (HelloMessage) context.getBean("message");
+        System.out.println(message.getMes());
     }
 }
